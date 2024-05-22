ca78ac11cb0e44b70c106dacb241bd703705c6df
Rifqi Naufal Tohari <rifqi.nufaltohari@gojek.com>
2024-05-22 10:48:11 +0700

    Initial commit

---
+++ src/main/kotlin/com/exercise/eurekaserver/EurekaserverApplication.kt
@@ -0,0 +1,11 @@
+package com.exercise.eurekaserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EurekaserverApplication
+
+fun main(args: Array<String>) {
+	runApplication<EurekaserverApplication>(*args)
+}
---
+++ src/test/kotlin/com/exercise/eurekaserver/EurekaserverApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.exercise.eurekaserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
